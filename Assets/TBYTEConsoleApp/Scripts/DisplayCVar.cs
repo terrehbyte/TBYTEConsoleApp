8633c6e497f9389b69a3ab26f337cb6e641883be
Terry Nguyen <terreh@terrehbyte.com>
2016-12-11 02:58:19 -0800

    Add example usage of CVar<T> in script

---
+++ Assets/TBYTEConsoleApp/Scripts/DisplayCVar.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TBYTEConsole;
+
+public class DisplayCVar : MonoBehaviour
+{
+    public Text label;
+    public string cvarName = "version";
+
+    void Update()
+    {
+        CVar<string> versionString = new CVar<string>(cvarName);
+
+        label.text = versionString.ToString();
+    }
+}
